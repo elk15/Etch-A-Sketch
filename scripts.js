cdb02a6fe0aa7992899c01cdd5e26d7b1bedfd89
elina <papadelina145@gmail.com>
2022-12-18 13:23:28 +0200

    Added basic functionality

--- scripts.js
+++ scripts.js
@@ -1,32 +1,65 @@
 const drawbox = document.querySelector('#drawbox');
-
-// for (let i = 1; i <= 256; i++) {
-//     const div = document.createElement('div');
-//     div.classList.add('square');
-//     drawbox.appendChild(div);
-// }
+const clearBtn = document.querySelector('#clearBtn');
+const colorPicker = document.querySelector('#colorPicker');
+const eraser = document.querySelector('#eraser');
+const rainbow = document.querySelector('#rainbow');
+let drawboxSize = 16;
+let color = 'black';
 
 function changeSize(size) {
+
     for (let i = 1; i <= size * size; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
         drawbox.appendChild(div);
-        document.querySelector('.square').style.setProperty('grid-template-columns', 'repeat(' + size + ', 1fr)');
-        document.querySelector('.square').style.setProperty('grid-template-rows', 'repeat(' + size + ', 1fr)');
+        drawbox.style.setProperty('grid-template-columns', 'repeat(' + size + ', 1fr)');
+
     }
+
+
 }
 
 
 function changeColor(color) {
-    squares.forEach((square) => {
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = color;
+    document.querySelectorAll('.square').forEach((square) => {
+        square.addEventListener('click', () => {
+            square.style.backgroundColor = color
 
         });
     });
 }
 
-changeSize(23);
-const squares = document.querySelectorAll('.square');
-changeColor('black');
+
+changeSize(drawboxSize);
+
+changeColor(color);
+
+
+clearBtn.addEventListener('click', () => {
+    document.querySelectorAll('.square').forEach((square) => {
+        drawbox.removeChild(square);
+    })
+    do {
+        drawboxSize = prompt('Select new size for canvas (3-64):')
+    } while (drawboxSize < 3 || drawboxSize > 64);
+    changeSize(drawboxSize);
+    changeColor('black');
+})
+
+colorPicker.addEventListener('input', (e) => {
+    console.log(e);
+    color = e.target.value;
+    changeColor(color);
+})
+
+eraser.addEventListener('click', () => {
+    changeColor('white');
+})
+
+
+
+
+
+
+
 
